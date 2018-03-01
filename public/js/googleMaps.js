09eed71beab4dbae5ed9fe6020fe92c77316c5e9
wknox021 <35344226+wknox021@users.noreply.github.com>
2018-03-01 10:47:26 -0800

    thirdpartyAPI lab

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,34 @@
+/*function initMap() {
+	// Create center marker at UCSD
+	var ucsd_ltlng = {lat:32.880, lng:-117.236};
+
+	// Create a map object and specify the DOM element for display
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: ucsd_ltlng,
+		zoom:15
+	});
+
+	var marker = new google.maps.Marker({
+		position: ucsd_ltlng,
+		map: map,
+		title: "UCSD"
+	});
+
+}*/
+
 function initMap() {
-	// add your code here
+
+	var ucsd_ltlng = {lat:32.879052, lng:-117.235923};
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: ucsd_ltlng,
+		zoom:15
+	});
+
+	var marker = new google.maps.Marker({
+		position: ucsd_ltlng,
+		map: map,
+		title: "UCSD"
+
+	});
 }
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -18,9 +18,39 @@ function initializePage() {
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function(){
+	/*$("div.box").bind("taplhold", tapholdHandler);
+	function tapholdHandler(event){
+		$(event.target).addClass("taphold");
+	}
+});*/
+	$(".judge-img").bind("taphold", tapholdHandler);
+
+	function tapholdHandler(event){
+		//get the id of the event source
+		var targetIDPrefix = event.target.id;
+		console.log("got prefix: " + targetIDPrefix);
+		//show bio
+		$("#" + targetIDPrefix + "-bio").show();
+	}
+});
+
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e) {
+  	e.preventDefault();
+  	console.log("submitting form...")
+  	var rsvpEmail = $('#rsvpEmail').val();
+  	$.post('addRSVP', {rsvpEmail: rsvpEmail}, postCallback);
+
+  });
+
+  function postCallback(res) {
+  	alert("RSVP for successfully submitted!");
+  	$('#rsvpEmail').val('');
+  }
 }
+
